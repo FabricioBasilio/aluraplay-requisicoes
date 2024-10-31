9ff6b678a1a99c3c6577b65eabf833a42dfefe34
Fabricio Basilio <fabriciobasilioo@gmail.com>
2024-10-31 17:27:57 -0300

    Criando a função de buscar vídeo

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -4,7 +4,34 @@ async function listarVideos() {
     return conexaoConvertida;
 }
 
+async function construirVideo(titulo, descricao, url, imagem) {
+    const conexao = await fetch("http://localhost:3000/videos", {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json"
+            // especificar o tipo de arquivo
+        },
+        body: JSON.stringify({
+            titulo: titulo,
+            descricao: `${descricao} mil visualizações`,
+            url: url,
+            imagem: imagem
+        })
+    });
+    // Sem segundo parâmetro o padrão é GET
+    const conexaoConvertida = await conexao.json();
+    return conexaoConvertida;
+}
+
+async function buscarVideo(busca) {
+    const conexao = await fetch(`http://localhost:3000/videos?q=${busca}`);
+    const conexaoConvertida = await conexao.json();
+
+    return conexaoConvertida;
+}
 
 export const conectaApi = {
-    listarVideos
+    listarVideos,
+    construirVideo,
+    buscarVideo
 }
